1770492431ed3a589424c19a52fdec000b622641
JunioDuarte <junioduartewg@gmail.com>
2022-08-05 21:11:54 -0300

    eloquent

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -15,17 +15,47 @@ class PostController extends Controller
     public function index()
     {
         echo "Listagem de Artigos!!";
-
-        $posts = Post::where('created_at', '>=', date('Y-m-d H:i:s'))->get();
-
-        foreach($posts as $post){
+/*
+        $posts = Post::where('created_at', '>=', date('Y-m-d H:i:s'))->orderBy('title', 'desc')->take(5
+        )->get();
+         foreach($posts as $post){
             echo"<h1>{$post->title}</h1>";
             echo"<h2>{$post->subtitle}</h2>";
             echo"<p>{$post->description}</p>";
             echo"<hr>";
+        } */
 
-        }
-        //var_dump($posts);
+
+
+        /* $post = Post::where('created_at', '>=', date('Y-m-d H:i:s'))->first();
+        echo"<h1>{$post->title}</h1>";
+        echo"<h2>{$post->subtitle}</h2>";
+        echo"<p>{$post->description}</p>";
+        echo"<hr>"; */
+
+
+
+        /* $post = Post::where('created_at', '>=', date('Y-m-d H:i:s'))->firstOrFail();
+        echo"<h1>{$post->title}</h1>";
+        echo"<h2>{$post->subtitle}</h2>";
+        echo"<p>{$post->description}</p>";
+        echo"<hr>"; */
+
+
+
+      /*   $post = Post::find(1);
+
+        echo"<h1>{$post->title}</h1>";
+        echo"<h2>{$post->subtitle}</h2>";
+        echo"<p>{$post->description}</p>";
+        echo"<hr>"; */
+
+
+        $posts = Post::all();
+        return view('posts.index', ['posts' => $posts]);
+
+
+        var_dump($post);
     }
 
     /**
